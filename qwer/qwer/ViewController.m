618ead5e47514b0e92c1afd75998381ee5c23dc7
JayFeng <fenghaijie@wochu.cn>
2019-04-02 13:52:02 +0800

    新增配置

--- qwer/qwer/ViewController.m
+++ qwer/qwer/ViewController.m
@@ -16,8 +16,20 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+   
+    
+ 
     // Do any additional setup after loading the view, typically from a nib.
-    NSLog(@"1111");
+    if (DebugSet == 0) {
+        UIAlertView *alterView = [[UIAlertView alloc]initWithTitle:@"000" message:nil delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"编辑该情景组",@"删除该情景组", nil];
+        
+        [alterView show];
+        
+    }else{
+        UIAlertView *alterView = [[UIAlertView alloc]initWithTitle:@"111" message:nil delegate:self cancelButtonTitle:@"取消" otherButtonTitles:@"编辑该情景组",@"删除该情景组", nil];
+        
+        [alterView show];
+    }
 }
 
 
